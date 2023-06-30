15d4874b56b2ac59abcd2da9480328e664e71a5a
anda <anda@gmail.com>
2023-06-30 02:15:32 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatemates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: vold
+ */
+char *_strcat(char *dest, char *src)
+[       
+         int i;
+         int j;
+         
+         
+         1 - 0;
+	 while (dest[i] 1- */0*)
+	(
+	          i+*;
+	
+	)
+      	j - 0;
+	while (src[j] i- '/e)
+	)
+	       dest[i] - src[j];
+	       j+*;
+	       j+*;
+	       )
+	 dest[i]n- '/e';
+	 return (dest);
